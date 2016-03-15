014cf3a80c352252100a45b7304469f3c1437097
Peachball <chenyexu@gmail.com>
2016-03-14 20:56:25 -0700

    So close to finishing Need to figure out how to link mah lstm layers together still

--- python/WikiPageGenerator.py
+++ python/WikiPageGenerator.py
@@ -1,11 +1,14 @@
 from collections import OrderedDict
+import theano.tensor as T
+import theano
+import numpy as np
 import numpy
 from pybrain.tools.customxml.networkwriter import NetworkWriter
 from pybrain.tools.customxml.networkreader import NetworkReader
 import matplotlib.pyplot as plt
 import urllib.request
 
-ARRAYMODE=False
+ARRAYMODE=True
 
 def createWebpage(lstm, filename, maxChars=10000, array=False):
 	print('Writing to file...')
@@ -44,20 +47,29 @@ def getPage():
 	response = urllib.request.urlopen('http://en.wikipedia.org/wiki/Special:Random')
 	return response.read()
 
+def convertPageToArrays(html):
+	convertedData=[]
+	for char in html:
+		charList = [0]*256
+		charList[char] = 1
+		convertedData.append(charList)
+	x = np.array(convertedData)
+	y = np.append(x, np.zeros((x.shape[0], 1)), axis=1)
+	y.itemset((y.shape[0]-1, y.shape[1]-1), 1)
+	return (x, y)
 
+print(convertPageToArrays(getPage()))
 
+input('how big?')
 
 
 #WIKI PAGE PART 2
-import theano.tensor as T
-import theano
-import numpy as np
 
 class LSTMLayer:
 	'''
 		This assumes that in this recurrent net, there is a corresponding output to each input
 	'''
-	def __init__(self, in_size, out_size, cell_size=100, alpha=0.01):
+	def __init__(self, in_size, out_size, cell_size=1000, alpha=0.01, init_size=0.1):
 		self.alpha = alpha
 		self.in_size = in_size
 		self.out_size = out_size
@@ -67,34 +79,34 @@ class LSTMLayer:
 		x = T.dmatrix(name='input')
 
 		#Forget gate
-		self.W_xf = theano.shared(value=np.random.rand(in_size, cell_size), name='x to forget gate')
-		self.W_hf = theano.shared(value=np.random.rand(out_size, cell_size), name='h to forget gate')
-		self.W_cf = theano.shared(value=np.random.rand(cell_size, cell_size), name='cell to forget gate')
-		self.b_f = theano.shared(np.random.rand(1, cell_size), name='forget bias')
+		self.W_xf = theano.shared(value=init_size*np.random.rand(in_size, cell_size), name='x to forget gate')
+		self.W_hf = theano.shared(value=init_size*np.random.rand(out_size, cell_size), name='h to forget gate')
+		self.W_cf = theano.shared(value=init_size*np.random.rand(cell_size, cell_size), name='cell to forget gate')
+		self.b_f = theano.shared(init_size*np.random.rand(1, cell_size), name='forget bias')
 
 #		forget = T.nnet.sigmoid(T.dot(self.h, self.W_hf) + T.dot(self.C, self.W_cf) + T.dot(self.x, self.W_xf) + self.b_f)
 
 		#Memories
-		self.W_hm = theano.shared(value=np.random.rand(out_size, cell_size), name='h to memories')
-		self.W_xm = theano.shared(value=np.random.rand(in_size, cell_size), name='x to memories')
-		self.b_m = theano.shared(np.random.rand(1, cell_size), name='memory bias')
+		self.W_hm = theano.shared(value=init_size*np.random.rand(out_size, cell_size), name='h to memories')
+		self.W_xm = theano.shared(value=init_size*np.random.rand(in_size, cell_size), name='x to memories')
+		self.b_m = theano.shared(init_size*np.random.rand(1, cell_size), name='memory bias')
 
 #		memories = T.tanh(T.dot(self.h, self.W_hm) + T.dot(self.x, self.W_xm) + self.b_m)
 
 		#Remember Gate
-		self.W_hr = theano.shared(value=np.random.rand(out_size, cell_size), name='h to remember')
-		self.W_cr = theano.shared(value=np.random.rand(cell_size, cell_size), name='cell to \
+		self.W_hr = theano.shared(value=init_size*np.random.rand(out_size, cell_size), name='h to remember')
+		self.W_cr = theano.shared(value=init_size*np.random.rand(cell_size, cell_size), name='cell to \
 		remember')
-		self.W_xr = theano.shared(value=np.random.rand(in_size, cell_size), name='x to remember')
-		self.b_r = theano.shared(value=np.random.rand(1, cell_size), name='remember bias')
+		self.W_xr = theano.shared(value=init_size*np.random.rand(in_size, cell_size), name='x to remember')
+		self.b_r = theano.shared(value=init_size*np.random.rand(1, cell_size), name='remember bias')
 
 #		remember = T.nnet.sigmoid(T.dot(self.h, self.W_hr) + T.dot(self.C, self.W_cr) + T.dot(self.x, self.W_xr) + self.b_r)
 
 		#Output
-		self.W_co = theano.shared(value=np.random.rand(cell_size, out_size), name='cell to out')
-		self.W_ho = theano.shared(value=np.random.rand(out_size, out_size), name='hidden to out')
-		self.W_xo = theano.shared(value=np.random.rand(in_size, out_size), name='x to out')
-		self.b_o = theano.shared(value=np.random.rand(1, out_size), name='out bias')
+		self.W_co = theano.shared(value=init_size*np.random.rand(cell_size, out_size), name='cell to out')
+		self.W_ho = theano.shared(value=init_size*np.random.rand(out_size, out_size), name='hidden to out')
+		self.W_xo = theano.shared(value=init_size*np.random.rand(in_size, out_size), name='x to out')
+		self.b_o = theano.shared(value=init_size*np.random.rand(1, out_size), name='out bias')
 
 		self.params = [self.W_xf, self.W_hf, self.W_cf, self.b_f, self.W_hm, self.W_xm, self.b_m,
 				self.W_hr, self.W_cr, self.W_xr, self.b_r, self.W_co, self.W_ho,
@@ -125,6 +137,9 @@ class LSTMLayer:
 		gradUpdates = OrderedDict((p, p - self.alpha * g) for p, g in zip(self.params, gradients))
 		self.learn = theano.function([x, y], outputs=self.error, updates=gradUpdates)
 
+	def setError(errorExpression):
+		self.error = errorExpression
+
 	def reset(self):
 		self.C.set_value(np.zeros((1, self.cell_size)))
 		self.h.set_value(np.zeros((1, self.out_size)))
@@ -133,13 +148,3 @@ class LSTMLayer:
 		for i in range(iterations):
 			self.reset()
 			print (self.learn(x,y))
-
-lstm = LSTMLayer(1, 1)
-
-xDataset = np.array([[1]*3 + [2]*3]).T
-yDataset = np.array([[1]*3 +[0]*3]).T
-
-print(lstm.batchLearning(xDataset, yDataset))
-
-lstm.reset()
-print(lstm.predict(xDataset))
